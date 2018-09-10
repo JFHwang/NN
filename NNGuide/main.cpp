47bfec63e3ad836f89858b0a36f92a28ee261596
John <John@DESKTOP-Q6CFVLB>
2018-09-09 21:31:03 -0400

    Code seems to work properly. In fact, more so than the C solution. Lots of code that goes off on a tangent. Will require refactoring.

--- NNGuide/main.cpp
+++ NNGuide/main.cpp
@@ -10,7 +10,7 @@
 
 
 
-int format(const char * filename, std::vector<std::array<float, 4>> feature, std::vector<std::string> flowertype) {
+int format(std::string filename, std::vector<std::array<float, 4>>& feature, std::vector<std::string>& flowerType) {
 
 	std::string line;
 	std::string substr;
@@ -32,12 +32,26 @@ int format(const char * filename, std::vector<std::array<float, 4>> feature, std
 
 		// Last (5th) value in line of csv is a string
 		std::getline(iss, substr);
-		flowertype.push_back(substr);
+		flowerType.push_back(substr);
 	}
 
 	infile.close();
+	std::cout << feature.size() << std::endl;
 
 	return 0;
+}
+
+
+float dist(std::array<float, 4> point1, std::array<float, 4> point2) {
+	float sum = 0;
+
+	if (point1.size() != point2.size()) { std::cerr << "ERROR: Number of features in train and test data don't match!" << std::endl; }
+
+	for (int i = 0; i < point1.size(); i++) {
+		sum += powf((point1[i] - point2[i]), 2);
+	}
+
+	return sqrt(sum);
 }
 
 
@@ -45,23 +59,82 @@ int format(const char * filename, std::vector<std::array<float, 4>> feature, std
 
 int main()
 {
-	std::vector<std::array<float, 4>> feature;
-	std::vector<std::string> flowertype;
+	const int K = 5;
+
+	std::vector<std::array<float, 4>> trainFeature;
+	std::vector<std::string> trainFlowerType;
+	std::vector<std::array<float, 4>> testFeature;
+	std::vector<std::string> testFlowerType;
 
 	// Move training data from csv to array.
-	format("iris.data.txt", feature, flowertype);
-
-	// Format test data here
+	format("iris.data.txt", trainFeature, trainFlowerType);
+	format("test.txt", testFeature, testFlowerType);
 
 	int correct = 0;
-	int total = 0;
+	int totalCases = 0;
 
-	// Put comparison code here
+	for (int testIndex = 0; testIndex < static_cast<int>(testFeature.size()); testIndex++) {
+		// Compare all elements in test data and train data
 
+		std::array<float, K> closestKDists;
+		for (int i = 0; i < K; i++) {
+			closestKDists[i] = 10'000;	// 10'000 is an arbitrarily high number
+		}
+		std::array<int, K> closestKTypes = { 0 };
 
-	printf("With %d samples, Results were %f%% correct\n", total, ((float)correct) / total * 100.0);
-	printf("Formatting complete\n");
+		for (int trainIndex = 0; trainIndex < static_cast<int>(trainFeature.size()); trainIndex++) {
+			
+			float distance = dist(trainFeature[trainIndex], testFeature[testIndex]);
+			int furthestIndex = 0;
+
+			// Find the furthest of the K nearest cases
+			for (int i = 1; i < K; i++) {
+				if (closestKDists[furthestIndex] < closestKDists[i])
+					furthestIndex = i;
+			}
+
+			// Check if the latest distance ranks in the top 5 closest ranks.
+			if (distance < closestKDists[furthestIndex]) {
+				closestKTypes[furthestIndex] = trainIndex;
+				closestKDists[furthestIndex] = distance;
+			}
+		}
+
+		// Count number of K votes which are correct.
+		int maxVotes = 0;
+		int maxVoteIndex = 0;
+		std::cout << "Votes are ";
+		for (int i = 0; i < K; i++) {
+			std::cout << trainFlowerType[closestKTypes[i]] << ", ";
+
+			// Count the number of votes that match vote i
+			int voteCount = 0;
+			for (int j = i + 1; j < K; j++) {
+				if (trainFlowerType[closestKTypes[i]].compare(trainFlowerType[closestKTypes[j]]) == 0) {
+					voteCount++;
+				}
+			}
+			// Update maxVotes
+			if(voteCount > maxVotes) {
+				voteCount = maxVotes;
+				maxVoteIndex = i;
+			}
+		}
+		std::string prediction = trainFlowerType[closestKTypes[maxVoteIndex]];
+		std::cout << " Prediction is " << prediction << " Answer is " << testFlowerType[testIndex] << std::endl;
+		// If the flower type of the test case matches the most similar flower type from training data, the prediction was correct!
+		if (prediction.compare(testFlowerType[testIndex]) == 0) {
+			correct++;
+			std::cout << "Test Case " << testIndex << " Correct!" << std::endl;
+		}
+		totalCases++;
+	}
+
+	// Calculate, print out, and display the results.
+	if (totalCases <= 0) { std::cerr << "ERROR: total cases amounts to " << totalCases << std::endl; std::cin.ignore(); return -1; }
+	std::cout << "Final Result: " << correct << " out of " << totalCases << " correct! Accuracy of " << static_cast<float>(correct) / totalCases * 100 << "%" << std::endl;
 	std::cin.ignore();
 
 	return 0;
 }
+
